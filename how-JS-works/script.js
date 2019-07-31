6c28c5b13218ee2e2801923e7d39a8a9201694f0
Luan Castheloge <luanccp@gmail.com>
2019-07-31 18:57:12 -0300

    [CORE] Understanding scoping and THIS keyword

--- how-JS-works/script.js
+++ how-JS-works/script.js
@@ -7,14 +7,11 @@ function calculateAge(year) {
     console.log(2016 - year);
 }
 
-// retirement(1956);
 var retirement = function(year) {
     console.log(65 - (2016 - year));
 }
 
-
 // variables
-
 console.log(age);
 var age = 23;
 
@@ -26,3 +23,37 @@ function foo() {
 foo();
 console.log(age)
 
+/**
+ * NOTES:
+ * - the only way to create a new scope in javascript is creating a function
+ * - Lexical scope: the javascript always look up to the parent scope and verify about the variables
+ */
+ 
+/** - THIS keyword always set the global context */
+
+console.log(this) //return window object
+ 
+ 
+ var test = {
+     name: "Luan",
+     yearOfBirth: 1991,
+     calculateAge: function() {
+         console.log(this) // return the object because this refer to parent object
+
+         /*function innerFunction(){
+             console.log(this) //return window object because is the parent object
+         }
+         innerFunction()*/
+     }
+ }
+ 
+ test.calculateAge()
+
+ //BORROWING
+
+ var mike = {
+    name: "Mike",
+    yearOfBirth: 1992
+ }
+ mike.calculateAge = test.calculateAge()
+ mike.calculateAge() // return mike object
