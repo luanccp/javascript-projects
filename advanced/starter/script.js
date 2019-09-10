08699ce9ec8f584c4fa118f36be5c8d73e705300
Luan Castheloge <luanccp@gmail.com>
2019-09-10 11:14:45 -0300

    [CORE] how to use bind

--- advanced/starter/script.js
+++ advanced/starter/script.js
@@ -121,3 +121,41 @@ interviewQuestion('teacher')('Mark');
 })(5);
 
 //we cannot access store out of the function
+
+/////////////////////////////
+// Bind, call and apply
+/*
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'teacher',
+    presentation: function(style, timeOfDay) {
+        if (style === 'formal') {
+            console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+        } else if (style === 'friendly') {
+            console.log('Hey! What\'s up? I\'m ' +  this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+        }
+    }
+};
+
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+};
+
+john.presentation('formal', 'morning');
+
+//Borrowing functions (change the this to this from Emily)
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+//john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+var johnFriendly = john.presentation.bind(john, 'friendly');
+
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
+*/
